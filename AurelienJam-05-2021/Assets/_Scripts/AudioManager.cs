2d220635a65249b057a7e5325096418cb8ceb0c2
Omadel <83096541+Omadel@users.noreply.github.com>
2021-05-10 10:57:13 +0200

    modifs

---
+++ AurelienJam-05-2021/Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager Instance { get; private set; }
+
+    [SerializeField] private Sound[] sounds = new Sound[0];
+
+    private void Awake() {
+        if(AudioManager.Instance == null) {
+            AudioManager.Instance = this;
+        } else {
+            Destroy(this.gameObject);
+            return;
+        }
+        this.transform.parent = null;
+        DontDestroyOnLoad(this.gameObject);
+
+        foreach(Sound sound in this.sounds) {
+            sound.Source = this.gameObject.AddComponent<AudioSource>();
+            sound.Source.clip = sound.Clip;
+
+            sound.Source.volume = sound.Volume;
+            sound.Source.pitch = sound.Pitch;
+            sound.Source.loop = sound.Loop;
+        }
+    }
+
+
+    public void Play(string name) {
+        Sound s = Array.Find(this.sounds, sound => sound.Name == name);
+        if(s == null) {
+            return;
+        }
+        s.Source.Play();
+    }
+    public void Stop(string name) {
+        Sound s = Array.Find(this.sounds, sound => sound.Name == name);
+        if(s == null) {
+            return;
+        }
+        s.Source.Stop();
+    }
+
+}
---
+++ AurelienJam-05-2021/Assets/_Scripts/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+    public string Name { get => this.name; set => this.name = value; }
+    public AudioClip Clip { get => this.clip; set => this.clip = value; }
+    public float Volume { get => this.volume; set => this.volume = value; }
+    public float Pitch { get => this.pitch; set => this.pitch = value; }
+    public bool Loop { get => this.loop; set => this.loop = value; }
+    public AudioSource Source { get => this.source; set => this.source = value; }
+
+    [SerializeField] private string name;
+    [SerializeField] private AudioClip clip;
+    [SerializeField, Range(0, 1)] private float volume = 1;
+    [SerializeField, Range(.1f, 3)] private float pitch = 1;
+    [SerializeField] private bool loop;
+
+    private AudioSource source;
+}
