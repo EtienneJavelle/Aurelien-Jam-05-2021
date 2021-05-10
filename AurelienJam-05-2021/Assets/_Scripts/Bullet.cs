5f60404f375e7f2742571711cd70324b6d1e5055
Omadel <83096541+Omadel@users.noreply.github.com>
2021-05-10 14:53:22 +0200

    Shoot Ability

---
+++ AurelienJam-05-2021/Assets/_Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    [SerializeField] private float speed = 6f;
+    [SerializeField] private float lifeTime = 5f;
+
+    private void OnEnable() {
+        GameObject.Destroy(this.gameObject, this.lifeTime);
+    }
+
+    private void Update() {
+        this.transform.position += this.transform.forward * this.speed * Time.deltaTime;
+    }
+}
