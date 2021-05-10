4b8d0fa864e201af2cb0e4d2d9e59e64674b2fa1
Omadel <83096541+Omadel@users.noreply.github.com>
2021-05-10 11:57:10 +0200

    Add Player

--- AurelienJam-05-2021/Assets/_Scripts/GameManager.cs
+++ AurelienJam-05-2021/Assets/_Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance { get; private set; }
+
     public AudioManager AudioManager { get => this.audioManager; }
 
     [SerializeField] private AudioManager audioManager = null;
---
+++ AurelienJam-05-2021/Assets/_Scripts/TurnTowardAimPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TurnTowardAimPosition : MonoBehaviour {
+
+    [SerializeField] private float turnSpeed = 500f;
+
+    private void LateUpdate() {
+
+        Vector3 mouse = Input.mousePosition;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(
+                                                            mouse.x,
+                                                            mouse.y,
+                                                            this.transform.position.y));
+        Vector3 forward = mouseWorld - this.transform.position;
+        //this.transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+        this.transform.LookAt(mouse);
+    }
+}
