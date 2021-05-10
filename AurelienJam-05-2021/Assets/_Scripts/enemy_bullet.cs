4001fc3f62ec1676698f34e8e9aa405924ff79b3
RomainDubar <romain.dubar@lacatholille.fr>
2021-05-10 21:21:07 +0200

    [GD/LD]- add LD v1 + equilibrage

---
+++ AurelienJam-05-2021/Assets/_Scripts/enemy_bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 6f;
+    [SerializeField] private float lifeTime = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+
+    private void OnEnable()
+    {
+        GameObject.Destroy(this.gameObject, this.lifeTime);
+    }
+
+    private void Update()
+    {
+        this.transform.position += this.transform.forward * this.speed * Time.deltaTime;
+    }
+}
