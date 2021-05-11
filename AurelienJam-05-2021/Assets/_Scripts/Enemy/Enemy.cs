66dd93e38803fd1857c7ae05e7fffc6ced62afe4
Omadel <83096541+Omadel@users.noreply.github.com>
2021-05-12 00:29:40 +0200

    Rename bulet prefab+ new move enemy

--- AurelienJam-05-2021/Assets/_Scripts/Enemy/Enemy.cs
+++ AurelienJam-05-2021/Assets/_Scripts/Enemy/Enemy.cs
@@ -6,34 +6,43 @@ public class Enemy : MonoBehaviour {
     [SerializeField, Range(0f, 1f)] private float turnSpeed = .5f;
 
     private void Awake() {
-        this.shooter = GetComponentInChildren<Shooter>();
-        this.shooter.enabled = false;
+        shooter = GetComponentInChildren<Shooter>();
+        shooter.enabled = false;
+        //rigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     public void Die() {
-        GameObject.Destroy(this.gameObject);
+        GameObject.Destroy(gameObject);
     }
 
-    private void Update() {
+    private void FixedUpdate() {
 
         Vector3 targetPos = GameManager.Instance.Player.transform.position;
 
-        Vector3 direction = targetPos - this.transform.position;
+        Vector3 direction = targetPos - transform.position;
         direction.Normalize();
-        this.transform.rotation = Quaternion.Slerp(
-            this.transform.rotation,
-            Quaternion.LookRotation(direction, this.transform.up),
-            this.turnSpeed);
 
-        if(Vector3.Distance(targetPos, this.transform.position) < this.shootRangeMin) {
-            this.transform.position -= direction.normalized * this.backSpeed * Time.deltaTime;
-        } else if(Vector3.Distance(targetPos, this.transform.position) < this.shootRangeMax) {
-            this.shooter.enabled = true;
+        if(Vector3.Distance(targetPos, transform.position) < shootRangeMin) {
+            controller.SimpleMove(-direction.normalized * backSpeed * Time.deltaTime);
+            //rigidbody.MovePosition(transform.position - (direction.normalized * backSpeed * Time.deltaTime));
+            //transform.position -= direction.normalized * backSpeed * Time.deltaTime;
+        } else if(Vector3.Distance(targetPos, transform.position) < shootRangeMax) {
+            shooter.enabled = true;
         } else {
-            this.transform.position += direction.normalized * this.forwardSpeed * Time.deltaTime;
-            this.shooter.enabled = false;
+            controller.SimpleMove(direction.normalized * forwardSpeed * Time.deltaTime);
+            //rigidbody.MovePosition(transform.position + (direction.normalized * forwardSpeed * Time.deltaTime));
+            //transform.position += direction.normalized * forwardSpeed * Time.deltaTime;
+            shooter.enabled = false;
         }
+        direction.y = 0;
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            Quaternion.LookRotation(direction, transform.up),
+            turnSpeed);
     }
 
+    private new Rigidbody rigidbody = null;
+    private CharacterController controller = null;
     private Shooter shooter = null;
 }
