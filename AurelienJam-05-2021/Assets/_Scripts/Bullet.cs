dc2255a44b751ac9dc5eba102b5bc38723fbe3a4
P3D\plejuste <paul.lejuste@lacatholille.fr>
2021-05-12 15:35:42 +0200

    [Art] -add- - Vfx explosion / missil / missil Old
    -fix-
    -Bullet script

--- AurelienJam-05-2021/Assets/_Scripts/Bullet.cs
+++ AurelienJam-05-2021/Assets/_Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour {
     [SerializeField] private float speed = 6f;
     [SerializeField] private float lifeTime = 5f;
     [SerializeField] private float growTime = .2f;
+    public GameObject Vfx_Explosion;
 
     private void OnTriggerEnter(Collider other) {
         if(other.TryGetComponent<Enemy>(out Enemy enemy)) {
@@ -12,6 +13,7 @@ public class Bullet : MonoBehaviour {
         } else if(other.TryGetComponent<DeathHandeler>(out DeathHandeler player)) {
             player.enabled = true;
         }
+        GameObject.Instantiate(Vfx_Explosion,transform.position, Quaternion.identity);
         GameObject.Destroy(gameObject);
     }
 
@@ -29,4 +31,6 @@ public class Bullet : MonoBehaviour {
     private void Update() {
         transform.position += transform.forward * speed * Time.deltaTime;
     }
+
+
 }
